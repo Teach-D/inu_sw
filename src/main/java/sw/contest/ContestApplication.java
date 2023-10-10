6e2727d9f0cf987fda1d39d15b87a5a0d17d8e8d
Teach-D <wkadht0619@gmail.com>
2023-10-10 12:45:30 +0900

    로그인 회원가입 페이지

--- src/main/java/sw/contest/ContestApplication.java
+++ src/main/java/sw/contest/ContestApplication.java
@@ -2,10 +2,9 @@ package sw.contest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class ContestApplication {
-
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})public class ContestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ContestApplication.class, args);
 	}
