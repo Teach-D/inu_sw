4a4bebbd5f53284da707a9cae386749c10021a78
wkadh <wkadht0619@gmail.com>
2024-02-22 20:51:20 +0900

    상품등록 서비스 개발 완료

--- src/main/java/sw/contest/spring/mvc2/shopping/DeliveryCode.java
+++ src/main/java/sw/contest/spring/mvc2/shopping/DeliveryCode.java
@@ -1,2 +1,13 @@
-package sw.contest.spring.mvc2.shopping;public class DeliveryCode {
+package sw.contest.spring.mvc2.shopping;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
 }
+
--- src/main/java/sw/contest/spring/mvc2/shopping/Item.java
+++ src/main/java/sw/contest/spring/mvc2/shopping/Item.java
@@ -1,2 +1,40 @@
-package sw.contest.spring.mvc2.shopping;public class Item {
+package sw.contest.spring.mvc2.shopping;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import java.util.List;
+
+@Data
+public class Item {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 100000)
+    private Integer price;
+
+    @NotNull
+    private Integer quantity;
+
+
+    private Boolean open;
+    private List<String> regions;
+    private ItemType itemType;
+    private String deliveryCode;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
